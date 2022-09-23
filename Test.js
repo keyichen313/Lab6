b995e98d200d5cdca9572bf4ca2e7ed4c71aec52
Dorcas Chen <ruthkeyi@gmail.com>
2022-09-22 21:38:57 -0700

    lab6

---
+++ Test.js
@@ -0,0 +1,19 @@
+function GreetingPrompt() {
+    var user = prompt("Enter your name, please: ", "Bach Lover");
+    if (user != null) {
+        alert("Greeting " + user + "!");
+    }
+  }
+function Choice(){
+    var choice = prompt("Do you like Bach?");
+    if (choice === "yes") {
+        alert("Yeah! I do as well!")
+    } else {
+        alert("You will like Bach!")
+    }
+} 
+
+  function Welcome() {
+    alert ("Welcome to the Bach's page!")
+  }
+
